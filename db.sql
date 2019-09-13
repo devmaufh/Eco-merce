95bfd4229d916064e735d80c088735f90f7e5d61
Mauricio Flores Hernandez <mau1361317@gmail.com>
2019-09-13 18:47:19 -0500

    First commit

---
+++ db.sql
@@ -0,0 +1,29 @@
+create table tipoUsuario(idTipo serial primary key, tipo varchar(25));
+create table proveedor(
+    idProvedor serial primary key,
+    rfc char(13),
+    nombre varchar(50),
+    direccion varchar(100),
+    provincia varchar(50),
+    email varchar(125),
+    telefono char(10),
+    cp varchar(5)
+);
+
+create table prodcutos(
+    idProd serial primary key,
+    nombre varchar(100),
+    descripcion varchar(500),
+    precioMayoreo number,
+    precioMenudeo number,
+    cantidad number,
+    idProvedor number,
+    foreign key(idProvedor) references proveedor(idProvedor)
+);
+create table usuarios(
+    idUser serial primary key,
+    idTipo number,
+    usuario varchar(125),
+    password varchar(125),
+    id_n number
+);
